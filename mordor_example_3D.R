eb4faaede90210a0b35d1a2c84c19c647c891412
Simon Brewer <brewersi@gmail.com>
2020-06-08 15:54:27 -0600

    Added elevation to 3D plots

--- mordor_example_3D.R
+++ mordor_example_3D.R
@@ -2,9 +2,23 @@
 
 library(rayshader)
 library(raster)
+library(rgdal)
+
+contour = readOGR("./data/vectors/Contours_18.shp")
+mincont = min(as.numeric(contour$Elevation), na.rm = TRUE)
+maxcont = max(as.numeric(contour$Elevation), na.rm = TRUE)
+rngcont = maxcont - mincont
 
 arda = raster("data/raster/10K.jpg")
 plot(arda)
+
+minelev = cellStats(arda, min)
+maxelev = cellStats(arda, max)
+
+## Stretch elevation
+arda = (arda / maxelev) * rngcont
+## sea level offset
+arda = arda - ((17 / maxelev) * rngcont)
 
 mordor = crop(arda, extent(c(1120000, 1600000, 400000, 750000)))
 # arda2 = arda2 * 10
@@ -15,20 +29,33 @@ mordor_mat = raster_to_matrix(mordor)
 
 ## 3D render (note the use of theta and phi to control angle and height)
 ## This opens an RGL-type window for zooming and panning
-mordor_mat %>%
-  sphere_shade(texture = "desert") %>%
-  add_water(detect_water(mordor_mat, max_height = 17), color = "desert") %>%
-  add_shadow(ray_shade(mordor_mat, zscale = 3), 0.5) %>%
-  add_shadow(ambient_shade(mordor_mat), 0) %>%
-  plot_3d(mordor_mat, zscale = 10, fov = 0, theta = 305, zoom = 0.75, phi = 25, windowsize = c(1000, 800))
+rgl::clear3d()
 
-## Render current view to RStudio plot window
-render_snapshot()
+mordor_mat %>% 
+  sphere_shade(texture = "imhof4") %>% 
+  # add_water(watermap, color = "imhof4") %>%
+  add_overlay(overlay_arr, alphalayer = 0.25) %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  plot_3d(elev_mat, zscale = zscale, windowsize = c(1200, 1000),
+          # water = TRUE, soliddepth = -max(elev_mat)/zscale, wateralpha = 0,
+          theta = 25, phi = 30, zoom = 0.65, fov = 60)
+
+## Set camera position
+render_camera(fov = 20, zoom = 0.15, theta = 235, phi = 10)
+
+# mordor_mat %>%
+#   sphere_shade(texture = "desert") %>%
+#   add_water(detect_water(mordor_mat, max_height = 17), color = "desert") %>%
+#   add_shadow(ray_shade(mordor_mat, zscale = 3), 0.5) %>%
+#   add_shadow(ambient_shade(mordor_mat), 0) %>%
+#   plot_3d(mordor_mat, zscale = 1, fov = 0, theta = 305, zoom = 0.75, phi = 25, windowsize = c(1000, 800))
 
 ## Render current view with added focal depth
 ## focus - controls point of focus
 ## length - controls field. Smaller values give a wider field
 render_depth(focus = 0.5, focallength = 35, clear = FALSE)
 
+render_depth(focus = 0.37, focallength = 35, clear = FALSE, filename = "mtdoom")
 
 
