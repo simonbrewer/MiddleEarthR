2f9a02f7a1ba62263fcd1d4550b397d1279a88fa
Simon Brewer <brewersi@gmail.com>
2020-06-08 14:47:53 -0600

    Script to add overlay for region (needs region_setup run first)

--- add_overlay.R
+++ add_overlay.R
@@ -3,60 +3,45 @@
 library(rayshader)
 library(raster)
 
-arda = raster("data/raster/10K.jpg")
-plot(arda)
-
 mext = extent(c(1120000, 1600000, 400000, 750000))
 
-## Get overlay
-overlay_stk <- stack("data/raster/overlay_72.png")
-## resample
-overlay_stk <- resample(overlay_stk, arda, method = "ngb")
-## Crop
-overlay_stk = crop(overlay_stk, mext)
-## Crop elevation
-mordor = crop(arda, overlay_stk)
-
-
-# arda2 = arda2 * 10
-plot(mordor)
-
-## Check extents
-compareRaster(overlay_stk, mordor, extent = FALSE, 
-              rowcol = TRUE, crs = FALSE, stopiffalse = FALSE)
-extent(overlay_stk)
-
-#And convert it to a matrix:
-mordor_mat = raster_to_matrix(mordor)
+load("region.RData")
 
 # calculate rayshader layers
-ambmat <- ambient_shade(mordor_mat, zscale = 30)
-raymat <- ray_shade(mordor_mat, zscale = 30, lambert = TRUE)
-watermap <- detect_water(mordor_mat, max_height = 17)
+ambmat <- ambient_shade(elev_mat, zscale = 30)
+raymat <- ray_shade(elev_mat, zscale = 30, lambert = TRUE)
+watermap <- detect_water(elev_mat, max_height = 17)
 
 # plot 2D
-mordor_mat %>%
+elev_mat %>%
   sphere_shade(texture = "imhof4") %>%
   add_water(watermap, color = "imhof4") %>%
   add_shadow(raymat, max_darken = 0.5) %>%
   add_shadow(ambmat, max_darken = 0.5) %>%
   plot_map()
 
-## Get overlay
-overlay_stk <- stack("data/raster/overlay_72.png")
-## Crop
-overlay_stk = crop(overlay_stk, mext)
+# 2D plot with map overlay
+elev_mat %>%
+  sphere_shade(texture = "imhof4") %>%
+  # add_water(watermap, color = "imhof4") %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  add_overlay(overlay_arr, alphalayer = 0.5) %>%
+  plot_map()
 
-## Check extents
-compareRaster(overlay_stk, mordor, extent = FALSE, 
-              rowcol = TRUE, crs = FALSE, stopiffalse = FALSE)
+zscale <- 10
+rgl::clear3d()
+elev_mat %>% 
+  sphere_shade(texture = "imhof4") %>% 
+  # add_water(watermap, color = "imhof4") %>%
+  add_overlay(overlay_arr, alphalayer = 0.25) %>%
+  add_shadow(raymat, max_darken = 0.5) %>%
+  add_shadow(ambmat, max_darken = 0.5) %>%
+  plot_3d(elev_mat, zscale = zscale, windowsize = c(1200, 1000),
+          # water = TRUE, soliddepth = -max(elev_mat)/zscale, wateralpha = 0,
+          theta = 25, phi = 30, zoom = 0.65, fov = 60)
 
-if (!compareRaster(r_rgb, elevation, extent = FALSE, rowcol = TRUE, crs = FALSE, stopiffalse = FALSE)) {
-  r_rgb <- setExtent(r_rgb, extent(elevation))
-}
-if (!compareRaster(r_rgb, elevation, extent = TRUE, rowcol = TRUE, crs = FALSE, stopiffalse = FALSE)) {
-  r_rgb_res <- resample(r_rgb, elevation, method = "ngb")
-  r_rgb_array <- as.array(r_rgb_res/255)
-} else {
-  r_rgb_array <- as.array(r_rgb/255)
-}
+## Set camera position
+render_camera(fov = 70, zoom = 0.4, theta = 25, phi = 35)
+
+render_snapshot()
--- region_setup.R
+++ region_setup.R
@@ -11,6 +11,7 @@ mext = extent(c(1120000, 1600000, 400000, 750000))
 
 ## Get overlay
 overlay_stk <- stack("data/raster/overlay_72.png")
+
 ## resample
 overlay_stk <- resample(overlay_stk, arda, method = "ngb")
 ## Crop overlay
