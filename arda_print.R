f39265868874fe6fb7ba0fe07df2053f81702c28
Simon Brewer <brewersi@gmail.com>
2020-06-11 09:08:35 -0600

    Added aggregate to raw print file

--- arda_print.R
+++ arda_print.R
@@ -11,6 +11,8 @@ maxcont = max(as.numeric(contour$Elevation), na.rm = TRUE)
 rngcont = maxcont - mincont
 
 arda = raster("data/raster/10K.jpg")
+arda = aggregate(arda, fact = 10)
+
 plot(arda)
 
 minelev = cellStats(arda, min)
@@ -19,14 +21,19 @@ maxelev = cellStats(arda, max)
 ## Stretch elevation
 arda = (arda / maxelev) * rngcont
 ## sea level offset
-arda = arda - ((17 / maxelev) * rngcont)
+# arda = arda - ((17 / maxelev) * rngcont)
+
+## Skirt offset
+arda = arda + 500
 
 #And convert it to a matrix:
 arda_mat = raster_to_matrix(arda)
 
+rgl::clear3d()
 arda_mat %>%
   sphere_shade() %>%
-  plot_3d(arda_mat,zscale=50)
+  plot_3d(arda_mat,zscale=250)
 
+render_camera(fov = 40, zoom = 0.25, theta = 35, phi = 30)
 
 # save_3dprint("montereybay_3d.stl", maxwidth = 150, unit = "mm")
